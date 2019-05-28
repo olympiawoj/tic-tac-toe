824de527eae9240682ed6b14adb21114358b5ab0
olympia <owojcik7@gmail.com>
2019-05-27 22:56:52 -0400

    Leave off at Lifting Up State

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -19,7 +19,21 @@ class Square extends React.Component {
   }
 }
 
+//constructor is a method thats called during creation of object from a class
+//it constructs our objects
+//its fires before the component is mounted
+//we take the props value from the constructor and pass it to the super method
+//when you call the super() method, it calls the constructor of the parent class which in the case of a React component is React.component
+
+//Array.fill() modifies all elements of an array from a start index (default 0) to an end index (array length), returns modified array
+//
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
   renderSquare(i) {
     return <Square value={i} />;
   }
