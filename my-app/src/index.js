e0a4c721b0166acef8509d7527c0f7296819a612
olympia <owojcik7@gmail.com>
2019-07-02 10:33:22 -0400

    Lifting State Up, Again - step 10

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,48 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Square extends React.Component {
-  //React components can have state by setting this.state in their constructors.
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
-//constructor is a method thats called during creation of object from a class
-//it constructs our objects
-//its fires before the component is mounted
-//we take the props value from the constructor and pass it to the super method
-//when you call the super() method, it calls the constructor of the parent class which in the case of a React component is React.component
-
-//Array.fill() modifies all elements of an array from a start index (default 0) to an end index (array length), returns modified array
-//
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null)
-    };
-  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = "Next player: X";
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -65,14 +44,72 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  state = {
+    history: [
+      {
+        squares: Array(9).fill(null)
+      }
+    ],
+    xIsNext: true
+  };
+
+  // handleClick(i) {
+  //   //grabs every board history
+  //   const history = this.state.history;
+  //   //grabs the latest item added to the list
+  //   const current = history[history.length - 1];
+  //   //creates copy of current array
+  //   const squares = current.squares.slice();
+  //   //if calculateWinner(squares) returns a winner OR there's a value in squares[i], then return that array
+  //   if (calculateWinner(squares) || squares[i]) {
+  //     return;
+  //   }
+  //   squares[i] = this.state.xIsNext ? "X" : "O";
+  //   this.setState({
+  //     //Within the Game’s handleClick method, we concatenate new history entries onto history.
+  //     history: history.concat([{ squares: squares }]),
+  //     xIsNext: !this.state.xIsNext
+  //   });
+  //   console.log("testing");
+  // }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status = "Winner " + winner;
+    } else {
+      status = "Next player " + (this.state.xIsNext ? "X" : "O");
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
@@ -83,3 +120,25 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
+
+//- If squares[a] has a value, and that value is equal to squares[b], and that value is equal to squares[c], *then* returns whatever is in  squares[a]  - "X", "O", or "null"
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
